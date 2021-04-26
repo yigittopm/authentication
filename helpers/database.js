36c0d0a212868b4b4acc2c2378436ac049cf90c0
yigittopm <yigittopm@hotmail.com>
2021-04-26 11:38:13 +0300

    async await connect db

--- helpers/database.js
+++ helpers/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-const connection = () => {
-    mongoose.connect(process.env.DB_URI, { useUnifiedTopology: true, useNewUrlParser: true})
+const connection = async () => {
+    await mongoose.connect(process.env.DB_URI, { useUnifiedTopology: true, useNewUrlParser: true})
         .then(() => console.log("Succcessful connect database.."))
         .catch(err => console.log(err));
 }
